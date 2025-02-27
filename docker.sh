5e8daac5f06b89f98b4573bccdf6fda1232d93e5
manupanand <manupanandindianinvestor@gmail.com>
2025-02-28 00:14:30 +0530

    new docker commands

--- docker.sh
+++ docker.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 # to run command along with redhat image
 sudo docker run docker.io/redhat/ubi9 env # runs env command environment variables
 
@@ -7,3 +9,20 @@ sudo docker run -e MONGO=true docker.io/redhat/ubi9 env
 #Remove Unused Containers, Images, Volumes, and Networks
 
 sudo docker system prune -a --volumes
+
+# clear builder cache 
+
+sudo docker builder prune -a
+
+#Remove sopped container
+sudo docker container prune
+
+#clear unused volume
+sudo docker volume prune 
+
+# check disk usage
+
+sudo docker system df
+
+# reboot will make container go off
+sudo docker -d --restart always -P container-name # --restart | always|no |never|on-failure
