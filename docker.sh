c222add2d93f9c47e5e3899940c8910ba7c9d3d5
manupanand <manupanandindianinvestor@gmail.com>
2025-02-28 00:03:23 +0530

    new codes

---
+++ docker.sh
@@ -0,0 +1,9 @@
+# to run command along with redhat image
+sudo docker run docker.io/redhat/ubi9 env # runs env command environment variables
+
+# -e env
+sudo docker run -e MONGO=true docker.io/redhat/ubi9 env
+
+#Remove Unused Containers, Images, Volumes, and Networks
+
+docker system prune -a --volumes
